6da91c316e6cd779b93e4ce687596d7006789039
MCPolichette <mark@polichette.com>
2018-07-27 10:10:03 -0600

    working on controller. .get works?

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,4 +1,17 @@
 // 3. Inside the `burgers_controller.js` file, import the following:
-
 //    * Express
+var express = require("express");
+
+var router = express.Router();
 //    * `burger.js`
+var burger = require("../models/burger.js");
+
+router.get("/", function (req, res) {
+    burger.all(function (data) {
+        var burger_object = {
+            burger: data
+        };
+        console.log(burger_object);
+        res.render("index", burger_object)
+    })
+})
